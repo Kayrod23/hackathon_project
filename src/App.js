72b05c5da7d134fe91d237857ee7c1c14033247f
Keeanu Rodriguez <keeanu7@gmail.com>
2023-07-22 15:54:04 -0400

    testing google maps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Home from './componets/Home';
 
 function App() {
+  const [pollLocations, setPollLocations] = useState(null)
+  useEffect (() => {
+    fetch(
+      `https://data.cityofnewyork.us/resource/utqd-4534.json`
+    )
+      .then((results) => results.json())
+      .then((response) => {
+        setPollLocations(response)
+      }).catch((error) => {
+        console.log(error);
+      })      
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div><Home pollLocations={pollLocations}/></div>
   );
 }
 
