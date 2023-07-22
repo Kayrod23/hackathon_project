5217d72efdb7d688074d8cd420b5867947e3393d
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-07-22 15:44:10 -0400

    added FAQ.js, Election.js, Voterinfo.js and a footer in components

---
+++ src/Pages/ElectionInfo.js
@@ -0,0 +1,7 @@
+export function ElectionInfo(){
+    <section className="DisplayInfo">
+        <p>Location: {ElectionInfo.ElectionLocation}</p>
+         <button type ="back" >Back</button>
+    </section> 
+   
+}
