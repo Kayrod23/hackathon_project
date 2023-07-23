35959e439ac957c2fb2c369432356f5889a16dc8
Jyoti Singh <jyo.singh88@gmail.com>
2023-07-23 13:13:54 -0400

    Styling Folder and Files

--- src/Pages/VoterInfo.js
+++ src/Pages/VoterInfo.js
@@ -1,10 +1,11 @@
-export function VoterInfo(){
-    <div>
-      <h2>Voter Information</h2>
-      <p>Requirements: {VoterInfo.VoterRequirements}</p>
+
+export default function VoterInfo() {
+  <div>
+    <h2>Voter Information</h2>
+    <p>Requirements: {VoterInfo.VoterRequirements}</p>
     <section className="userInfo">
-         <button type ="back" >Back</button>
-        
-    </section> 
-    </div>
+      <button type="back">Back</button>
+    </section>
+  </div>;
 }
+
