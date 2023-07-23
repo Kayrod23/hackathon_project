af72ccd891b1223196570b4f5c09845f31ce4eaa
Keeanu Rodriguez <keeanu7@gmail.com>
2023-07-23 04:33:36 -0400

    chechk

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home.jsx";
-import Search from "./Pages/Search";
 
 function App() {
-  const [pollLocations, setPollLocations] = useState(null)
-useEffect (() => {
-  fetch(
-    `https://data.cityofnewyork.us/resource/utqd-4534.json`
-  )
-    .then((results) => results.json())
-    .then((response) => {
-      setPollLocations(response)
-    }).catch((error) => {
-      console.log(error);
-    })      
-}, [])
-
   return (
     <div className="Nav">
       <BrowserRouter>
       <Navbar />
           <Routes>
             {/* Define your routes here */}
-            <Route path="/" element={<Home pollLocations={pollLocations} />} />
-            <Route path="/search" element={<Search />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </BrowserRouter>
     </div>
