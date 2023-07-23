d008e4f49fc215fd586699575f3260a9f387fe32
touhami-ben <brooklyn.ben76@gmail.com>
2023-07-23 13:42:44 -0400

    connect voter info to an external web site

--- src/Pages/VoterInfo.js
+++ src/Pages/VoterInfo.js
@@ -1,10 +1,16 @@
 
 export default function VoterInfo() {
+
+  
   return (
     <div>
       <h2>Voter Information</h2>
       <p>Requirements: {VoterInfo.VoterRequirements}</p>
-      <section className="userInfo"></section>
+      <section className="userInfo">
+       <a href="https://www.elections.ny.gov/NYSBOE/download/voting/voteregform-eng-fillable.pdf">
+       New York State Voter Registration Form
+       </a>
+      </section>
     </div>
   );
 }
