e544aa7ef0072eb14dcce7514028b5fbf54adda4
Keeanu Rodriguez <keeanu7@gmail.com>
2023-07-23 21:36:56 -0400

    check

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -2,7 +2,7 @@ import { useMemo } from "react";
 import { GoogleMap, MarkerF, useLoadScript} from "@react-google-maps/api";
 
 function Map({pollLocations, user}) {
-    const center = useMemo(() => ({ lat: 40.8, lng: -73.79 }), []);
+    const center = useMemo(() => ({ lat: 4.8, lng: -73.79 }), []);
     console.log(user)
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_API,
@@ -12,9 +12,9 @@ function Map({pollLocations, user}) {
             <h1> Loading...</h1>
         </div>);
     return (
-      <GoogleMap zoom={8} center={user ? user : center} mapContainerClassName="map-container">
+      <GoogleMap zoom={8} center={user} mapContainerClassName="map-container">
         {user ? console.log(user) : console.log(center)}
-          {pollLocations ?
+          {pollLocations ? 
           pollLocations.filter((location)=>location.location).map((location, index) =>{
                   let lat = parseFloat(location.location.latitude)
                   let lng = parseFloat(location.location.longitude)
